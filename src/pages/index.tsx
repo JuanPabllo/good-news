06dcf396dd01ebe304a8ce79ba3e1a9a637d0a16
juanpabllo <juanpablo192.dreaw@gmail.com>
2021-07-31 13:39:14 -0300

    page exemplo and styles global

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Good News</title>
+      </Head>
+      <h1>Hello World</h1>
+    </>
+  );
+}
