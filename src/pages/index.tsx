2e86e75de131b781bf57738ecc4fbd9c418d271d
juanpabllo <juanpablo192.dreaw@gmail.com>
2021-07-31 15:41:29 -0300

    page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,30 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import style from './home.module.scss';
 export default function Home() {
   return (
     <>
       <Head>
         <title>Good News</title>
       </Head>
-      <h1>Hello World</h1>
+
+      <main className={style.contentContainer}>
+        <section className={style.hero}>
+          <span>👋 Hey, welcome</span>
+          <h1>
+            News About
+            <br /> the <span>Happy</span> World.
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
