9721069a8d5777281a082d8f65d003343b3122ca
juanpabllo <juanpablo192.dreaw@gmail.com>
2021-08-07 13:26:45 -0300

    redirect user if not subscriber on page post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -45,7 +45,15 @@ export const getServerSideProps: GetServerSideProps = async ({
 }) => {
   const session = await getSession({ req });
   const { slug } = params;
-  // if(!session) {}
+
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
 
   const prismic = getPrismicClient(req);
 
